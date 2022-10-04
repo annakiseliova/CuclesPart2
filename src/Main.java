53fb3d73642191313ebd45e5616d0a265e62246e
annakiseliova <annakiseliova7@gmail.com>
2022-10-04 16:09:58 +0300

    Циклы. Часть 2. Домашнее задание 2. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Циклы. Часть 2. Домашнее задание 2. Задание 2");
         int contribution = 15_000;
         int month = 1;
-        while (contribution <= 12_000_000) {
+        while (month <= 108) {
             contribution = contribution + contribution * 7 / 100;
             if (month % 6 == 0) {
                 System.out.println("Месяц " + month + ", сумма накоплений " + contribution);
