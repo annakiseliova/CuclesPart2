17e19fc31105be76125dfd622d03f239c19c640e
annakiseliova <annakiseliova7@gmail.com>
2022-10-04 16:24:02 +0300

    Циклы. Часть 2. Домашнее задание 2. Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Циклы. Часть 2. Домашнее задание 2. Задание 2
-        System.out.println("Циклы. Часть 2. Домашнее задание 2. Задание 2");
-        int contribution = 15_000;
-        int month = 1;
-        while (month <= 108) {
-            contribution = contribution + contribution * 7 / 100;
-            if (month % 6 == 0) {
-                System.out.println("Месяц " + month + ", сумма накоплений " + contribution);
+        // Циклы. Часть 2. Домашнее задание 2. Задание 4
+        System.out.println("Циклы. Часть 2. Домашнее задание 2. Задание 4");
+        int friday = 4;
+        int date = 1;
+        while (date < 31) {
+            date = date + 1;
+            if (date == friday) {
+                System.out.println("Сегодня пятница, " + date + " число. Необходимо подготовить отчет");
+                friday = friday + 7;
             }
-            month = month + 1;
         }
     }
 }
