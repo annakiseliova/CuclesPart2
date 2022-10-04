23579494ec7f01429c11f0f35a8e0d13f93306bf
annakiseliova <annakiseliova7@gmail.com>
2022-10-04 16:00:46 +0300

    Циклы. Часть 2. Домашнее задание 2. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Циклы. Часть 2. Домашнее задание 1. Задание 3
-        System.out.println("Циклы. Часть 2. Домашнее задание 1. Задание 3");
-        int population = 12_000_000;
-        int fertilityPerOneThousand = 17;
-        int mortalityPerOneThousand  = 8;
-        int year = 1;
-        int fertilityPerYear = (population/1000)*fertilityPerOneThousand;
-        int mortalityPerYear = (population/1000)*mortalityPerOneThousand;
-        while (year <= 10) {
-            population = population + fertilityPerYear - mortalityPerYear;
-            System.out.println("Год " + year + ", численность населения составляет " + population);
-            year = year + 1;
+        // Циклы. Часть 2. Домашнее задание 2. Задание 1
+        System.out.println("Циклы. Часть 2. Домашнее задание 2. Задание 1");
+        int contribution = 15_000;
+        int month = 0;
+        while (contribution <= 12_000_000) {
+            contribution = contribution + contribution * 7 / 100;
+            System.out.println("Месяц " + month + ", сумма накоплений " + contribution);
+            month = month + 1;
         }
     }
 }
